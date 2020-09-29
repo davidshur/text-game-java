26c65da7bdc59c3e0832596598753a6c6ccd12ac
davidshur <shurdave@gmail.com>
2020-09-29 15:52:25 -0700

    rm vscode settings

--- TextAdventure/src/com/findshur/Game.java
+++ TextAdventure/src/com/findshur/Game.java
@@ -1,8 +1,16 @@
 package com.findshur;
 
+import java.util.Scanner;
+
 public class Game {
   public static void main(String[] args) {
-    var parser = Parser.getInstance("This is the only parser.");
-    System.out.println(parser.value);
+    var parser = Parser.getInstance();
+    var in = new Scanner(System.in);
+
+    var input = in.nextLine();
+    var command = parser.parse(input);
+
+    System.out.println(command);
+    in.close();
   }
 }
--- TextAdventure/src/com/findshur/Parser.java
+++ TextAdventure/src/com/findshur/Parser.java
@@ -1,13 +1,14 @@
 package com.findshur;
 
 import com.findshur.Enums.Articles;
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.HashSet;
 
 /**
- * Class which is made in the Singleton design pattern. There can be only one
- * Parser instance. This singleton is safe for use in multithreaded environments
- * due to the synchronizing of the threads.
+ * Parser made in the Singleton design pattern. This singleton is safe for use
+ * in multithreaded environments due to the synchronizing of the threads.
  */
 public final class Parser {
   private static volatile Parser instance;
@@ -15,20 +16,42 @@ public final class Parser {
   private EnumSet<Articles> articles = EnumSet.allOf(Articles.class);
   private HashSet<Articles> articlesHashSet = new HashSet<Articles>(articles);
 
-  private Parser(String value) {
-    this.value = value;
-  }
-
-  public static Parser getInstance(String value) {
+  public static Parser getInstance() {
     Parser result = instance;
     if (result != null) {
       return result;
     }
     synchronized (Parser.class) {
       if (instance == null) {
-        instance = new Parser(value);
+        instance = new Parser();
       }
       return instance;
     }
+  }
+
+  /**
+   * Takes a user command string and parses it to a user command array.
+   * 
+   * @param userInput String user has entered as a command.
+   * @return ArrayList<Integer> that has been cleaned, reduced and translated to
+   *         represent a user method.
+   */
+  public ArrayList<String> parse(String userInput) {
+    var commandArray = cleanStringToArrayList(userInput);
+    return commandArray;
+  }
+
+  /**
+   * Takes user command and then cleans, formats, and splits it to an array list
+   * from where it can be used in the parsing process.
+   * 
+   * @param s User command as string.
+   * @return ArrayList<String> of user command that has been cleaned and split to
+   *         words containing only lower-case letters and no symbols.
+   */
+  private ArrayList<String> cleanStringToArrayList(String s) {
+    var a = s.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+    var l = new ArrayList<String>(Arrays.asList(a));
+    return l;
   }
 }
