b56c0b779fb3b931ba07d8af62c7e71473e12fd8
davidshur <shurdave@gmail.com>
2020-09-30 07:39:14 -0700

    add basic game play for bug testing parser.

--- TextAdventure/src/com/findshur/Game.java
+++ TextAdventure/src/com/findshur/Game.java
@@ -3,14 +3,52 @@ package com.findshur;
 import java.util.Scanner;
 
 public class Game {
+  private static boolean playing = true;
+  private static String introText = "Welcome to Freep!";
+  private static String endingText = "The end...";
+  private static String roomText = "There is a cave opening to the North. On the floor there is scattering of papers. You are surrounded by a redwood forest on all other sides.";
+
   public static void main(String[] args) {
-    var parser = Parser.getInstance();
     var in = new Scanner(System.in);
+    printGameText(introText);
 
-    var input = in.nextLine();
-    var command = parser.parse(input);
+    while (playing) {
+      printGameText(roomText);
+      var input = in.nextLine();
+      if (input.equals("stop")) {
+        parseAndPrintUserText("Stop game.");
+        break;
+      }
+      parseAndPrintUserText(input);
+    }
 
-    System.out.println(command);
     in.close();
+    printGameText(endingText);
+  }
+
+  /**
+   * Prints styled game text and descriptions.
+   * 
+   * @param text A string to be printed stylistically.
+   */
+  private static void printGameText(String text) {
+    System.out.println("~".repeat(10));
+    System.out.println();
+    System.out.println(text);
+    System.out.println();
+  }
+
+  /**
+   * Prints parsed user command for debugging.
+   * 
+   * @param s User input string from the scanner.
+   */
+  private static void parseAndPrintUserText(String s) {
+    var command = Parser.parse(s);
+
+    System.out.println();
+    System.out.println("Parsed command:");
+    System.out.println(command);
+    System.out.println();
   }
 }
--- TextAdventure/src/com/findshur/Parser.java
+++ TextAdventure/src/com/findshur/Parser.java
@@ -10,25 +10,24 @@ import java.util.EnumSet;
 import java.util.HashSet;
 
 /**
- * Parser made in the Singleton design pattern. This singleton is safe for use
- * in multithreaded environments due to the synchronizing of the threads.
+ * Parser made in the Singleton design pattern.
  */
 public final class Parser {
   // create hashset of articles
-  private EnumSet<Articles> articles = EnumSet.allOf(Articles.class);
-  private HashSet<Articles> articlesHashSet = new HashSet<Articles>(articles);
+  private static EnumSet<Articles> articles = EnumSet.allOf(Articles.class);
+  private static HashSet<Articles> articlesHashSet = new HashSet<Articles>(articles);
   // create hashset of prepositions
-  private EnumSet<Prepositions> prepositions = EnumSet.allOf(Prepositions.class);
-  private HashSet<Prepositions> prepositionsHashSet = new HashSet<Prepositions>(prepositions);
+  private static EnumSet<Prepositions> prepositions = EnumSet.allOf(Prepositions.class);
+  private static HashSet<Prepositions> prepositionsHashSet = new HashSet<Prepositions>(prepositions);
   // create hashset of dictionary
-  private EnumSet<Dictionary> dictionary = EnumSet.allOf(Dictionary.class);
-  private HashSet<Dictionary> dictionaryHashSet = new HashSet<Dictionary>(dictionary);
+  private static EnumSet<Dictionary> dictionary = EnumSet.allOf(Dictionary.class);
+  private static HashSet<Dictionary> dictionaryHashSet = new HashSet<Dictionary>(dictionary);
 
   private static volatile Parser instance;
 
   /**
-   * Creates an instance of the Parser in a way that will ensure there is only one
-   * instance of Parser possible... even in a multi-threading scenario.
+   * Creates an instance of the Parser ensures Singleton pattern even in
+   * multi-threading situation.
    * 
    * @return new Parser instance.
    */
@@ -52,7 +51,7 @@ public final class Parser {
    * @return ArrayList<Integer> that has been cleaned, reduced and translated to
    *         represent a user method.
    */
-  public ArrayList<String> parse(String userInput) {
+  public static ArrayList<String> parse(String userInput) {
     var commandArray = cleanStringToArrayList(userInput);
 
     removeArticles(commandArray);
@@ -62,14 +61,13 @@ public final class Parser {
   }
 
   /**
-   * Takes user command and then cleans, formats, and splits it to an array list
-   * from where it can be used in the parsing process.
+   * Cleans, formats, and splits a string to an array list.
    * 
-   * @param s User command as string.
-   * @return ArrayList<String> of user command that has been cleaned and split to
-   *         words containing only upper-case letters and no symbols.
+   * @param s A string.
+   * @return Array list that has been cleaned and split to words containing only
+   *         upper-case letters and no symbols.
    */
-  private ArrayList<String> cleanStringToArrayList(String s) {
+  private static ArrayList<String> cleanStringToArrayList(String s) {
     var a = s.replaceAll("[^a-zA-Z ]", "").toUpperCase().split("\\s+");
     var l = new ArrayList<String>(Arrays.asList(a));
     return l;
@@ -80,26 +78,42 @@ public final class Parser {
    * 
    * @param l ArrayList<String> to have articles removed from.
    */
-  private void removeArticles(ArrayList<String> l) {
+  private static void removeArticles(ArrayList<String> l) {
     l.removeIf(word -> articlesHashSet.toString().contains(word));
   }
 
   /**
    * Creates a compound verb by combining the verb with a preposition found next
-   * or last in the command list. Cleans up command list afterwards.
+   * or last in the command array list. Cleans up command array list afterwards.
    * 
    * @param l An ArrayList of strings.
    * @return An ArrayList of strings with a compound verb instead of a verb and
    *         separate preposition, if possible.
    */
-  private void createAnyCompoundVerb(ArrayList<String> l) {
+  private static void createAnyCompoundVerb(ArrayList<String> l) {
     if (prepositionsHashSet.toString().contains(l.get(l.size() - 1))) {
       l.add(1, l.get(l.size() - 1));
       l.remove(l.size() - 1);
     }
     if (prepositionsHashSet.toString().contains(l.get(1))) {
-      l.set(0, l.get(0) + " " + l.get(1));
+      l.set(0, l.get(0) + l.get(1));
       l.remove(1);
     }
+  }
+
+  /**
+   * Takes an array list of words and maps it to an array list of integers.
+   * 
+   * @param wordList An array list of strings in the format [verb, dictionary] or
+   *                 [verb, dictionary, preposition, dictionary] where dictionary
+   *                 can be more than one word representing a game object (i.e.
+   *                 ugly cave troll).
+   * @return An array of integers which will have a length of either 2 or 4
+   *         depending on if a preposition was used mid sentence.
+   */
+  private static int[] mapStringsToIntegers(ArrayList<String> stringList) {
+    int[] intList = new int[4];
+
+    return intList;
   }
 }
