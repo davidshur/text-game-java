12f948f7533d35670776d5fd3e64b167d5c006bf
davidshur <shurdave@gmail.com>
2020-10-02 16:14:45 -0700

    rename room to location

--- TextAdventure/src/com/findshur/Location.java
+++ TextAdventure/src/com/findshur/Location.java
@@ -2,10 +2,15 @@ package com.findshur;
 
 import java.util.HashMap;
 
-public class Map {
+/**
+ * Simple map for the adventure. Makes room objects. Has a function to return
+ * the next room, given a room object.
+ */
+public class Location {
+  private static Room currentLocation;
   private static HashMap<Room, Room> map = new HashMap<Room, Room>();
 
-  public Map() {
+  public Location() {
     var entranceRoom = new Room(Rooms.ENTRANCE, Rooms.CHEST, "Cave Entrance",
         "You are standing outside a cave. The breeze is cold and biting. Ahead of you is the only shelter for miles. It looks like there is only one way to go.");
     var chestRoom = new Room(Rooms.CHEST, Rooms.TROLL, "Cavern",
@@ -13,13 +18,29 @@ public class Map {
     var trollRoom = new Room(Rooms.TROLL, Rooms.CHEST, "Troll Room",
         "A large and vicious troll stands before you. It sneers and bangs on its chest. Snot drips from its nose.");
 
+    currentLocation = entranceRoom;
+
     map.put(entranceRoom, chestRoom);
     map.put(chestRoom, trollRoom);
     map.put(trollRoom, chestRoom);
   }
 
-  public static Room nextRoom(Room room) {
-    var nextRoom = map.get(room);
+  /**
+   * Gets the next room using the current room.
+   * 
+   * @return The room object of the next room based on current location.
+   */
+  public static Room nextLocation() {
+    var nextRoom = map.get(currentLocation);
     return nextRoom;
+  }
+
+  /**
+   * Gets the current room.
+   * 
+   * @return The room object of the current room.
+   */
+  public static Room getLocation() {
+    return currentLocation;
   }
 }
