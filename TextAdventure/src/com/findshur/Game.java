8df1d18cabbe70c47741a61912cd63b8d29a64da
davidshur <shurdave@gmail.com>
2020-10-07 15:31:28 -0700

    cleanup game class and refactor

--- TextAdventure/src/com/findshur/Game.java
+++ TextAdventure/src/com/findshur/Game.java
@@ -3,51 +3,62 @@ package com.findshur;
 import java.util.Scanner;
 
 public class Game {
+  private static Scanner in = new Scanner(System.in);
+  private static Player player = new Player();
   private static boolean playing = true;
-  private static String introduction = "Welcome to the dungeon. You may say: go, get, or fight.";
 
   public static void main(String[] args) {
-    var in = new Scanner(System.in);
-    var player = Player.getInstance();
-
-    printGameText(introduction);
-    while (playing) {
-      printGameText(player.getCurrentRoom().getDescription());
-      var input = in.nextLine();
-      var command = Parser.parse(input);
-      System.out.println();
-      switch (command) {
-        case "GO":
-          Player.move();
-          break;
-        case "GET":
-          Player.get();
-          break;
-        case "FIGHT":
-          Player.attack();
-          break;
-        default:
-          System.out.println("Valid commands are: go, get, or fight.");
-          System.out.println();
-      }
-    }
-
-    in.close();
-  }
-
-  /**
-   * Prints styled game text and descriptions.
-   * 
-   * @param text A string to be printed stylistically.
-   */
-  private static void printGameText(String text) {
-    System.out.println("~".repeat(10));
-    System.out.println();
-    System.out.println(text);
-    System.out.println();
+    doGameIntro();
+    playGame();
+    doGameTeardown();
   }
 
   public static void endGame() {
     playing = false;
+  }
+
+  private static void doGameIntro() {
+    printStyledText("Welcome to the dungeon. You may say: go, get, or fight.");
+  }
+
+  private static void playGame() {
+    while (playing) {
+      printStyledText(player.getCurrentRoom().getDescription());
+      getAndRunCommand();
+    }
+  }
+
+  private static void doGameTeardown() {
+    in.close();
+  }
+
+  private static String getCommand() {
+    var command = Parser.parse(in.nextLine());
+    System.out.println();
+    return command;
+  }
+
+  private static void getAndRunCommand() {
+    switch (getCommand()) {
+      case "GO":
+        Player.move();
+        break;
+      case "GET":
+        Player.get();
+        break;
+      case "FIGHT":
+        Player.attack();
+        break;
+      default:
+        System.out.println("Valid commands are: go, get, or fight.");
+        System.out.println();
+    }
+  }
+
+  private static void printStyledText(String text) {
+    System.out.println("~".repeat(10));
+    System.out.println();
+    System.out.println(text);
+    System.out.println();
   }
 }
--- TextAdventure/src/com/findshur/Parser.java
+++ TextAdventure/src/com/findshur/Parser.java
@@ -1,48 +1,11 @@
 package com.findshur;
 
-/**
- * Parser made in the Singleton design pattern.
- */
 public final class Parser {
-  private static volatile Parser instance;
-
-  /**
-   * Creates an instance of the Parser ensures Singleton pattern even in
-   * multi-threading situation.
-   * 
-   * @return the Parser instance.
-   */
-  public static Parser getInstance() {
-    Parser result = instance;
-    if (result != null) {
-      return result;
-    }
-    synchronized (Parser.class) {
-      if (instance == null) {
-        instance = new Parser();
-      }
-      return instance;
-    }
-  }
-
-  /**
-   * Takes a command and formats it.
-   * 
-   * @param command A string to be parsed.
-   * @return The first word of the command.
-   */
   public static String parse(String command) {
     String[] formattedCommand = formatString(command);
     return formattedCommand[0];
   }
 
-  /**
-   * Formats and splits a string to an array.
-   * 
-   * @param s A string.
-   * @return Array that has been formatted and split to words containing only
-   *         upper-case letters and no symbols.
-   */
   private static String[] formatString(String s) {
     var a = s.replaceAll("[^a-zA-Z ]", "").toUpperCase().split("\\s+");
     return a;
